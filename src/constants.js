e8c0d9f4aa629d93aac7b7e28154695cb1494943
Rudy Dahbura <rdahbura@gmail.com>
2018-08-27 19:07:10 -0700

    Added SSL.

--- src/constants.js
+++ src/constants.js
@@ -4,8 +4,8 @@ export const AUTH_CONFIG = {
   domain: 'rudydahbura.auth0.com',
   domainAlias: 'idp.letsdoauth.com',
   clientId: 'lsHbUXlIE1d8SLWgOZBCfBL8SbmRCRc-',
-  callbackUrl: 'http://localhost:3000/callback',
-  logoutUrl: 'http://localhost:3000',
+  callbackUrl: 'https://localhost:3000/callback',
+  logoutUrl: 'https://localhost:3000',
   apiAudience: 'https://api.letsdoauth.com',
   apiUrl: 'http://localhost:3001'
 };
