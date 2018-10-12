45288ae63fcf8f9d6282d912ad53a667b018adf9
Rudy Dahbura <rdahbura@gmail.com>
2018-10-12 14:24:01 -0700

    Updated for react scripts 2.x.x.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
 
 import configureStore from './store/configureStore';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 const store = configureStore();
 
-render(<App store={store} />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
